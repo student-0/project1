2568a0090433e747fab3da483d5231a20c5c46a4
student-0 <bam@abcdunlimited.com>
2015-10-24 10:49:17 -0400

    Update p1zzz.java

--- p1zzz.java
+++ p1zzz.java
@@ -1,6 +1,37 @@
 //// Project 1
 //// Zero Zero Zero
 
-void setup() {
+String title=  "Project 1";
+String author= "Zero Zero Zero";
+
+//// SETUP:  size
+void setup() {    
   size(640,480);
 }
+
+//// NEXT FRAME:  scene, action, messages.
+void draw() {
+  scene();
+  action();
+  messages();
+}
+
+void scene() {
+}
+void action() {
+}
+void messages() {
+  grading();
+}
+
+
+//// GRADING ////
+void grading() {
+  if (key == '#') {
+    background(255,255,200);
+    text( "GRADING", )
+  }
+}
+  
+
+
